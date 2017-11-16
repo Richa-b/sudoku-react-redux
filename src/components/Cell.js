4403548d8291d0c7c7b4718dcd54fe3e9e54f33d
Richa <richa@tothenew.com>
2017-11-16 08:21:12 +0530

    Added validation to allow numbers between 1-9 in sudoku

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -2,23 +2,35 @@ import React, {Component} from 'react';
 
 class Cell extends Component {
 
+    constructor() {
+        super();
+        this.onInputChange = this.onInputChange.bind(this)
+    }
+
+    onInputChange(e) {
+        e.preventDefault()
+        let {cell, onChangeCellValue} = this.props;
+        let value = e.target.value
+
+        if (cell.isEditable && (value !== '' && /^[1-9]$/.test(value))) {
+            cell.value = value
+            onChangeCellValue(cell)
+        }
+    }
+
     render() {
         let {cell, onChangeCellValue} = this.props;
         let classes = []
         cell.isEditable ? classes.push('editable') : classes.push('non-editable')
         cell.hasConflict ? classes.push('has-conflict') : classes.push('')
 
-        console.log(cell.hasConflict)
         classes.push('ci' + cell.columnIndex)
         classes.push('ri' + cell.rowIndex)
 
         return (
             <td key={cell.columnIndex} className={classes.join(' ')}>
-                <input type='text' onChange={(e) => {
-                    cell.value = e.target.value
-                    return onChangeCellValue(cell)
-                }
-                } readOnly={!cell.isEditable} value={cell.value}/>
+                <input type='text' onChange={this.onInputChange}
+                       readOnly={!cell.isEditable} value={cell.value}/>
             </td>)
     }
 }
